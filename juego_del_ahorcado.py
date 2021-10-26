bbbe9904399826dead40e72539fe1f3ae7d9b4f7
Bruno Hernández <brunohervi93@gmail.com>
2021-10-25 22:37:40 -0500

    Funcionalidad de juego de ahorcado finalizada

---
+++ juego_del_ahorcado.py
@@ -0,0 +1,69 @@
+import random
+import os
+
+def read():
+    words = []
+    with open("./archivos/data.txt","r", encoding="utf-8") as f:
+        [words.append(line) for line in f]
+    return words
+
+
+def randword(words):
+    word_random= random.choice(words)
+    a,b = 'áéíóúü','aeiouu'
+    trans = str.maketrans(a,b)
+    word_random = word_random.translate(trans)
+    word_random= word_random.upper()
+    return word_random
+
+
+def initialize(word_random):
+    size = len(word_random)
+    size = size - 1
+    sep_word_random = {i:word_random[i] for i in range(size)}
+    missing_letters = {i: "_" for i in range(size)}
+    return sep_word_random, missing_letters
+
+
+def process(initialize_dict):
+    sep_word_random = initialize_dict[0]
+    missing_letters = initialize_dict[1]
+    
+
+    while missing_letters != sep_word_random:
+        os.system("cls")
+        print('¡Adivina la palabra!')
+
+        for values in missing_letters.values():
+            print(values + " ", end = "")
+
+        print('\n')
+    #Agregar aquí el manejo de errores       
+        letter = input('Ingresa una letra: ')
+        letter = letter.upper()
+
+        for key, value in sep_word_random.items():
+            if letter == value:
+                missing_letters[key] = letter
+    
+    return missing_letters
+   
+    
+def end(word_random):
+
+    os.system("cls")
+    print('¡Felicidades! Has ganado el juego, la palabra era: ', word_random )         
+
+    
+
+def run():
+
+    words = read()
+    word_random = randword(words)
+    initialize_dict = initialize(word_random)
+    process(initialize_dict)
+    end(word_random)
+
+
+if __name__ == '__main__':
+    run()
